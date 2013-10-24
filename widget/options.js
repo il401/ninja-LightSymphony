d406a6d2e039cbdcc59e787e42a8c30122986166
Ian Lumb <mail@ian-lumb.com>
2013-10-24 19:37:08 +0100

    Big edits

--- widget/options.js
+++
@@ -1,8 +0,0 @@
-return {
-  "name": "LightSymphony",
-  "sizeX": 1, // Not yet implemented
-  "sizeY": 1, // Not yet implemented
-  "deviceMap": [
-    { "deviceId": [1011, 1012], "minimum": 1, "maximum": 1}
-  ]
-}
